9a350a5b3559b49205bb7071ef27a3dc2eedd7fe
Fernando Antunes <fernando.antunes1@gmail.com>
2020-11-18 10:30:16 -0300

    Exercicio 2

--- src/Dog.jsx
+++ src/Dog.jsx
@@ -19,15 +19,29 @@ class Dog extends React.Component {
     this.fetchDog();
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.imgUrl.includes('terrier')) return false;
+    return true;
+  }
+
+  componentDidUpdate(prevPros, prevState) {
+    if (this.state.imgUrl !== '' && this.state.imgUrl !== prevState.imgUrl) {
+      const dogArray = this.state.imgUrl.split('/');
+      alert(dogArray[4].toUpperCase());
+    }
+  }
+
   async fetchDog() {
     this.setState({ loadStatus: true },
       async () => {
         const response = await fetch('https://dog.ceo/api/breeds/image/random');
         const responseJson = await response.json()
         this.setState(({ imagesList }) => ({
+          imgUrl: responseJson.message,
           imagesList: [...imagesList, responseJson.message],
           loadStatus: false,
         }));
+        localStorage.setItem('imgUrl', this.state.imgUrl);
       })
   }
 
